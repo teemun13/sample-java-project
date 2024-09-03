121aa420f8d0db81ba093226a3b4d01f0e41edcc
teemun13 <timuntee0613@gmail.com>
2024-09-03 23:06:03 +0800

    updated files

--- src/main/java/com/example/SampleJavaProject.java
+++ src/main/java/com/example/SampleJavaProject.java
@@ -16,13 +16,15 @@ import lombok.Setter;
  */
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter 
+@Setter 
 public class SampleJavaProject implements Runnable {
 
     /** The delay between printed messages. */
     private static final long PRINT_DELAY = 1000L;
 
     /** The name to be printed in the output message. */
-    @Getter @Setter @NonNull
+    @NonNull
     @Parameter(names = "--name", description = "set the user's name",
                required = true)
     private String name = "world";
